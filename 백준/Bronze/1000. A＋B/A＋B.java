54a3214a74474b4e4e964daa3afeb8bb2df16363
전하늘(Haneul Jeon) <146147196+haneuljj@users.noreply.github.com>
2024-05-10 18:30:08 +0900

    [Bronze V] Title: A+B, Time: 216 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a, b;
+        int answer;
+
+        Scanner scanner = new Scanner(System.in);
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        answer = a + b;
+
+        System.out.println(answer);
+
+        scanner.close();
+    }
+}
