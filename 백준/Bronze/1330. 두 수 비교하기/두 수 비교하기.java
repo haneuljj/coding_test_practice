6f162d797548564dacc2fd29b58db0ba04f0e2d2
전하늘(Haneul Jeon) <146147196+haneuljj@users.noreply.github.com>
2024-05-16 13:59:33 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if (a > b)
+            System.out.println(">");
+        else if (a < b)
+            System.out.println("<");
+        else if (a == b)
+            System.out.println("==");
+
+        sc.close();
+    }
+}
