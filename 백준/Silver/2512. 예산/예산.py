a57f2f3a88674826e67d358fa277204c696eaac3
전하늘(Haneul Jeon) <146147196+haneuljj@users.noreply.github.com>
2024-06-03 14:50:51 +0900

    [Silver II] Title: 예산, Time: 92 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.py
@@ -0,0 +1,23 @@
+N = int(input())
+jibang = list(map(int, input().split()))
+budget = int(input())
+
+# 탐색범위 설정
+left = 0
+right = max(jibang)
+answer = -1
+
+while left <= right:
+    middle = (left + right) // 2
+
+    sum = 0
+    for i in range(N):
+        sum += min(middle, jibang[i])
+
+    if sum <= budget:
+        answer = middle
+        left = middle + 1
+    else:
+        right = middle - 1
+
+print(answer)
